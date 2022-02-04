bc1ee776f8081ff9e1fe5d5146e46ac0756317f6
sekhar76405 <ssjio5402@gmail.com>
2022-02-04 23:46:40 +0530

    ReverseLinkedList Day - 3

--- Easy/LinkedLists/ReverseLinkedList.cpp
+++ Easy/LinkedLists/ReverseLinkedList.cpp
@@ -2,7 +2,7 @@
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 */
 
-// easy approach O(N) + O(N)
+// easy stack approach O(N) + O(N)
 // use a stack and fill it with one traversal thorough list, in the next traversal update node values by poping the stack everytime.
 
 ListNode* reverseList(ListNode* head) {
@@ -22,3 +22,25 @@ ListNode* reverseList(ListNode* head) {
         return head;
 }
 
+
+// easy recursive approach O(N) + O(N)(recursion stack)
+// write edgecases first, let recursion reverse the list from 2nd node, save the address of reversed list in new_head
+// Now just iterate from new_head till end and place 1st node that head is pointing and make its next null return new_head; 
+ListNode* reverseList(ListNode* head) {
+        if(head == NULL)    // no node
+            return head;
+        if(head->next == NULL)  //single node
+            return head;
+        
+        ListNode* new_head = reverseList(head->next);
+        ListNode* temp = new_head;
+        
+        while(temp->next != NULL)
+            temp = temp->next;
+        temp->next = head;
+        head->next = NULL;
+        head = new_head;
+        return head;
+ }
+
+ 
